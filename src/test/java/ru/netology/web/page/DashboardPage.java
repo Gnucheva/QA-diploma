529a5f1e95b84983f192245952fe224c8a69192a
Anastasia <n41111215@mail.ru>
2024-04-06 13:43:51 +0300

    edit depricated method

--- src/test/java/ru/netology/web/page/DashboardPage.java
+++ src/test/java/ru/netology/web/page/DashboardPage.java
@@ -1,9 +1,6 @@
 package ru.netology.web.page;
 
-import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
-
-import java.time.Duration;
 
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.$$;
